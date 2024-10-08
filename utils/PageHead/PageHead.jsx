c59fe3f3d536b2f55b0eb44bd8d794d13ec4ab7a
ValeriiViachalo <valeraviachalo@gmail.com>
2024-10-08 11:31:54 +0300

    seo fixed(try.2)

--- utils/PageHead/PageHead.jsx
+++ utils/PageHead/PageHead.jsx
@@ -7,8 +7,8 @@ export const PageHead = ({ data }) => {
       <meta name="og:title" content={data.documentTitle} />
       <meta name="description" content={data.documentDescription} />
       <meta property="og:image" content={data.documentImage} />
-      <meta property="og:image:width" content="720" />
-      <meta property="og:image:height" content="405" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <meta property="og:image:alt" content="RTRTS" />
     </Head>
   );
